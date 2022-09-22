e526e6d05cb86a1d68b376143b2fb463b274b2b9
Taylor Wagstaff <taylorwagstaff@gmail.com>
2022-09-22 17:27:16 +1200

    tested url req params in routes

--- server/db/messages.js
+++ server/db/messages.js
@@ -1,17 +1,16 @@
 const connection = require('./connection')
 
-function getMessage(db = connection) {
-  let input = { lat: -36.8645, long: 174.7765 }
+function getMessage(input, db = connection) {
   const r = 0.0005
   return db('messages')
     .whereBetween('lat', [input.lat - r, input.lat + r])
     .whereBetween('long', [input.long - r, input.long + r])
     .select('msg')
-    .then((messages) => console.log(messages))
+
 }
 
 module.exports = {
   getMessage,
 }
 
-getMessage(connection)
+// let input = { lat: -36.8645, long: 174.7765 }
